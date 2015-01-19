96106fdc4cc16d4385c75781fddb809af4c9ccd5
Michael "Jake" Ajemian <jake@luck-e-charm.com>
2015-01-18 23:33:31 -0500

    Update HeadsetButtonsReceiver.java
    Streamlined code that manages key events related to headset hook, media play/pause, media next, media previous.  If a good event is discovered, construct an event and send a broadcast via the context, with the Action field of the Intent set to the current value.

--- ACEMusicPlayer/src/main/java/com/aniruddhc/acemusic/player/BroadcastReceivers/HeadsetButtonsReceiver.java
+++ ACEMusicPlayer/src/main/java/com/aniruddhc/acemusic/player/BroadcastReceivers/HeadsetButtonsReceiver.java
@@ -41,59 +41,37 @@ public class HeadsetButtonsReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
     			
     	mApp = (Common) context.getApplicationContext();
-    	
-    	//There's no point in going any further if the service isn't running.
-    	if (mApp.isServiceRunning()) {+
+
+		// Required: Service has to be running for the check to run.    	
+    	if (mApp.isServiceRunning()) {
     			
-			//Aaaaand there's no point in continuing if the intent doesn't contain info about headset control inputs.
-			String intentAction = intent.getAction();
-			if (!Intent.ACTION_MEDIA_BUTTON.equals(intentAction)) {
+			// Return if the intent doesn't contain info about headset control inputs.
+			if (!Intent.ACTION_MEDIA_BUTTON.equals( intent.getAction )) {
 				return;
 			}
 			
 			KeyEvent event = (KeyEvent) intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
-			int keycode = event.getKeyCode();
-			int action = event.getAction();
-			
-			//Switch through each event and perform the appropriate action based on the intent that's ben
-			if (keycode == KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE || keycode == KeyEvent.KEYCODE_HEADSETHOOK) {
-				
-				if (action == KeyEvent.ACTION_DOWN) {
-					//Toggle play/pause.
-					Intent playPauseIntent = new Intent();
-					playPauseIntent.setAction(AudioPlaybackService.PLAY_PAUSE_ACTION);
-					context.sendBroadcast(playPauseIntent);
-					
-				}
-				
-			}
-			
-			if (keycode == KeyEvent.KEYCODE_MEDIA_NEXT) {
-				
-				if (action == KeyEvent.ACTION_DOWN) {
-					//Fire a broadcast that skips to the next track.
-					Intent nextIntent = new Intent();
-					nextIntent.setAction("com.aniruddhc.acemusic.player.NEXT_ACTION");
-					context.sendBroadcast(nextIntent);
-					
-				}
-				
-			}
-			
-			if (keycode == KeyEvent.KEYCODE_MEDIA_PREVIOUS) {
-				
-				if (action == KeyEvent.ACTION_DOWN) {
-					//Fire a broadcast that goes back to the previous track.
-					Intent previousIntent = new Intent();
-					previousIntent.setAction("com.aniruddhc.acemusic.player.PREVIOUS_ACTION");
-					context.sendBroadcast(previousIntent);
-					
-				}
-				
-			}
-	        
+
+        	if( event.getAction() == KeyEvent.ACTION_DOWN ) {
+            	Intent intent = new Intent();
+
+        		switch ( event.getKeyCode() ) {
+            		case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
+            		case KeyEvent.KEYCODE_HEADSETHOOK:
+                		intent.setAction(AudioPlaybackService.PLAY_PAUSE_ACTION);
+                		break;
+
+            		case KeyEvent.KEYCODE_MEDIA_NEXT:
+                		intent.setAction("com.aniruddhc.acemusic.player.NEXT_ACTION");
+                		break;
+
+            		case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
+                		intent.setAction("com.aniruddhc.acemusic.player.PREVIOUS_ACTION");
+                		break;
+        		}
+
+            	context.sendBroadcast(intent);
+        	}
     	}
-    	
     }
-    
 }
